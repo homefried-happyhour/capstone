5c41d2df3015e64402d5f88a8587740cb3c3d7b7
OnyxFire1 <alexneil179@gmail.com>
2022-07-13 18:35:49 -0700

    started show page

--- app/javascript/components/pages/LastCallShow.js
+++ app/javascript/components/pages/LastCallShow.js
@@ -1,9 +1,42 @@
 import React, { Component } from 'react'
-
+import {
+    Card,
+    CardImg,
+    CardBody,
+    CardTitle,
+    CardSubtitle,
+    CardText,
+    Button
+  } from 'reactstrap'
+  import { NavLink } from 'react-router-dom'
+  
 export default class LastCallShow extends Component {
     render() {
         return (
-            <div>LastCallShow</div>
-        )
-    }
+            <>
+            <div className="page-body">
+          
+            <Card body className="card-show">
+              <CardImg top width="100%" src={cat.image} />
+              <CardBody>
+                <CardTitle>Hi, my name is {cat.name}</CardTitle>
+                <CardSubtitle>{cat.age}</CardSubtitle>
+                <CardText>{cat.enjoys}</CardText>
+                <NavLink to={`/catedit/${cat.id}`}>
+                  <Button>Edit Cat Profile</Button>
+                </NavLink>
+                <br />
+                <br />
+                <NavLink to="/catindex">
+                  <Button onClick={() => this.props.deleteCat(cat.id)}>
+                    Delete Cat Profile
+                  </Button>
+                </NavLink>
+              </CardBody>
+            </Card>
+          
+        </div>
+      </>
+    )
+  }
 }
