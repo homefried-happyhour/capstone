78fb4961e7f51252fdc2029ff23a87004350b714
OnyxFire1 <alexneil179@gmail.com>
2022-07-20 11:59:21 -0700

    Started Not-Found page

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Card, Button, Row } from 'react-bootstrap'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+    
+    <Card id="about-card" >
+            
+              <Card.Text>
+<h1 "text-align:center">
+                You look thirsty, click on the header and find yourself a drink.
+                </h1>
+              </Card.Text>
+            
+          </Card>
+    </>
   )
 }
