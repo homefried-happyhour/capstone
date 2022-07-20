298c57971358c561321a82834a5095b9ff062d52
michael-a-tobias <michael.a.tobias@outlook.com>
2022-07-20 13:37:12 -0700

    NotFound page complete

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -4,16 +4,9 @@ import { Card, Button, Row } from 'react-bootstrap'
 export default function NotFound() {
   return (
     <>
-    
-    <Card id="about-card" >
-            
-              <Card.Text>
-<h1 "text-align:center">
-                You look thirsty, click on the header and find yourself a drink.
-                </h1>
-              </Card.Text>
-            
-          </Card>
+      <h1 id="about-card">
+        You look thirsty, click on the header and find yourself a drink.
+      </h1>
     </>
   )
 }
