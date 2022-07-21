7fb74a2143b97a7fef80d639e47c5ec9eae38c0d
vjtovar <vjtovar25@gmail.com>
2022-07-21 13:46:38 -0700

    devise-styling-views

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Navbar, Button } from 'react-bootstrap'
-
+import React from "react";
+import { Navbar, Button } from "react-bootstrap";
 
 export default function Footer() {
   return (
     <>
-      <Navbar className="color-nav" fixed="bottom" collapseOnSelect expand="lg" variant="dark">
-          <p id="copyright" style={{color:"white"}}>&copy; Homefried-Happy-Hour 2022</p>
-          <Button className="mx-auto" variant="outline-light" href="/about">About Us</Button>
-          
+      <Navbar
+        className="color-nav"
+        fixed="bottom"
+        collapseOnSelect
+        expand="lg"
+        variant="dark"
+      >
+        <p id="copyright" style={{ color: "white" }}>
+          &copy; Homefried-Happy-Hour 2022
+        </p>
+        <Button className="mx-auto" variant="outline-light" href="/about">
+          About Us
+        </Button>
       </Navbar>
     </>
-  )
+  );
 }
